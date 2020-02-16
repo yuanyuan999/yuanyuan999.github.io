25d8b74a5d40bada90e4f0b469bda8bc20b091c2
yuanyuan999 <1461103297@qq.com>
2020-02-16 13:27:34 +0800

    Site updated: 2020-02-16 13:27:34

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -1,17 +1,3 @@
-// 浏览器搞笑标题
-var OriginTitle = document.title;
-var titleTime;
-document.addEventListener('visibilitychange', function () {
-    if (document.hidden) {
-        $('[rel="icon"]').attr('href', "https://www.yuanpeng666.top/wp-content/uploads/2019/09/1547489545506.png");
-        document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
-        clearTimeout(titleTime);
-    }
-    else {
-        $('[rel="icon"]').attr('href', "https://www.yuanpeng666.top/wp-content/uploads/2019/09/1547489545506.png");
-        document.title = '(ฅ>ω<*ฅ) 噫又好啦 ~' + OriginTitle;
-        titleTime = setTimeout(function () {
-            document.title = OriginTitle;
-        }, 2000);
-    }
-});
+// build time:Sun Feb 16 2020 13:25:56 GMT+0800 (CST)
+var OriginTitle=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","https://www.yuanpeng666.top/wp-content/uploads/2019/09/1547489545506.png");document.title="╭(°A°`)╮ 页面崩溃啦 ~";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","https://www.yuanpeng666.top/wp-content/uploads/2019/09/1547489545506.png");document.title="(ฅ>ω<*ฅ) 噫又好啦 ~"+OriginTitle;titleTime=setTimeout(function(){document.title=OriginTitle},2e3)}});
+//rebuild by neat 
